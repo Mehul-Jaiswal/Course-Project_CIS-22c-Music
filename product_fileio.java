39aa79f7d7388e9af239c317132d8a4066afcf17
Brandon Lee <brandonlee2002@gmail.com>
2022-03-18 14:24:52 -0700

    Update product_fileio.java

--- product_fileio.java
+++ product_fileio.java
@@ -1,1 +1,15 @@
+import java.io.*;
+public class product_fileio {
 
+	
+	public static void main(String args[]) throws IOException {
+		File file = new File ("Object1.txt");
+		
+		file.createNewFile();
+		
+		FileReader test = new FileReader(file);
+		
+		Product [] array = new Product[80];
+		
+	}
+}
