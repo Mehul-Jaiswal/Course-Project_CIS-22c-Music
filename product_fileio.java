ce9bec26e22f0d44b0ec2d8b1b59824bd0e15d91
Mehul-Jaiswal <87316002+Mehul-Jaiswal@users.noreply.github.com>
2022-03-18 16:01:12 -0700

    Update product_fileio.java

--- product_fileio.java
+++ product_fileio.java
@@ -13,3 +13,34 @@ public class product_fileio {
 		
 	}
 }
+
+
+/*
+public static BST<Product> readfile()
+	{
+		boolean readable = true;
+		BufferedReader buff;
+		FileReader filereader;
+
+		try {
+			filereader = new FileReader("/Users/mehuljaiswal/eclipse-workspace/Course_project_cis22c/src/products.txt");
+			buff = new BufferedReader(filereader);
+			String line;
+
+			while (readable) {
+				BST<Product> products = new BST<Product>();
+				line = buff.readLine();
+				if (line == null) // finished reading
+				{
+					readable = false;
+					break;
+				}
+				
+				
+			}
+			buff.close();
+		} catch (IOException e) {
+			System.out.println("readfile(): Problem reading file. " + e.toString());
+		}
+	}
+*/
