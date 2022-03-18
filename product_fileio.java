b3e24ac7dda0ba9d1aac016da88ca692ac02ae0a
Brandon Lee <brandonlee2002@gmail.com>
2022-03-18 16:22:24 -0700

    Update product_fileio.java

--- product_fileio.java
+++ product_fileio.java
@@ -17,32 +17,38 @@ public class product_fileio {
 
 /*
 public static BST<Product> readfile()
-	{
-		boolean readable = true;
-		BufferedReader buff;
-		FileReader filereader;
-		try {
-			filereader = new FileReader("/Users/mehuljaiswal/eclipse-workspace/Course_project_cis22c/src/products.txt");
-			buff = new BufferedReader(filereader);
-			String line;
-			while (readable) {
-				BST<Product> products = new BST<Product>();
-				line = buff.readLine();
-				if (line == null) // finished reading
-				{
-					readable = false;
-					break;
-				} else {
-					String[] words = line.split(",");
-					for (int i = 0; i < words.size; i++) {
-						products.insert(words[i]);
-					}
-				}
-				
-			}
-			buff.close();
-		} catch (IOException e) {
-			System.out.println("readfile(): Problem reading file. " + e.toString());
-		}
-	}
+    {
+        boolean readable = true;
+        BufferedReader buff;
+        FileReader filereader;
+        BST<Product> products_name = new BST<Product>();
+        BST<Product> products_id = new BST<Product>();
+        try {
+            filereader = new FileReader("/Users/mehuljaiswal/eclipse-workspace/Course_project_cis22c/src/products.txt");
+            buff = new BufferedReader(filereader);
+            String line;
+
+            while (readable) {
+
+                line = buff.readLine();
+                if (line == null) // finished reading
+                {
+                    readable = false;
+                    break;
+                }
+
+                String[] vertices = line.split(",");
+                //System.out.println(vertices[0]);
+                // String name, String productId, String singer, double cost, double duration, String release_year, String genre, int numInStock
+                products_name.insert(vertices[0]);
+                products_id.insert(vertices[1]);
+            }
+            //System.out.println(products_name.inOrderString().toString());
+            //System.out.println(products_id.inOrderString().toString());
+            buff.close();
+        } catch (IOException e) {
+            System.out.println("readfile(): Problem reading file. " + e.toString());
+        }
+        return products_name;
+    }
 */
